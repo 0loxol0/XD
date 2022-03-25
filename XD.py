5b8d2a9065e9c595e4ae775d51e4ab40bea9d9e5
Ahmed Husen Al dukali <72076217+Ahmedalzwage@users.noreply.github.com>
2022-03-25 18:26:05 +0200

    Add files via upload

--- XD.py
+++ XD.py
@@ -579,7 +579,7 @@ def publik():
     print "\n\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] Type \'me\' Crack From Friendlist"
     idt = raw_input('\x1b[0;96m\x1b[0;97m [\x1b[1;36m\xe2\x80\xa2\x1b[1;37m] User ID Target: ')
     try:
-        r = requests.get('https://graph.facebook.com/v2.0/' + idt + '?fields=friends.limit(5000)&access_token=' + toket)
+        r = requests.get('https://graph.facebook.com/' + idt + '?fields=friends.limit(5000)&access_token=' + toket)
         z = json.loads(r.text)
         for a in z["friends"]['data']:
             idne = a['id']
